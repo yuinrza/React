0dbd7e89819214a4eda3de58e006a7b9ce1557db
yunirza garcia <garcia.yunilds@gmail.com>
2022-12-06 12:08:53 -0300

    primer commit

---
+++ my-contactos/src/App.js
@@ -0,0 +1,246 @@
+import React, { useEffect,useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from "reactstrap";
+import Filtrados from './components/filtrados';
+
+const data = [ 
+  {id: 1, nombre: "Ankys Serrano",  email: "ankys@yuni.com", telefono: "20198682"},
+  {id: 2, nombre: "Jose Lisandro Ruiz",  email: "jose@yuni.com", telefono: "20198773"},
+  {id: 3, nombre: "Ruth Piter", email: "ruth@yuni.com", telefono: "20198625"},
+];
+
+
+
+/*
+
+const [data] = useState([])
+useEffect(()=> {
+  fetch('http://localhost:3000/contatos')
+  .then((res) => res.json())
+  .then(
+    (resultado)=> {
+      data(resultado)
+    }
+  )
+  }
+)
+*/
+
+class App extends React.Component {
+  state = {
+    data: data,
+    form: {
+      id: "",
+      nombre: "",
+      email: "",
+      telefono: "",
+    }, 
+    dataSearch: [],
+    modalInsertar: false,
+    modalActualizar: false,
+    modalConfirmacion: false
+  };
+
+  handleChange = e =>{
+    this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+ 
+  //#region metodos de modales
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  mostrarConfirmacion = (dato) => {
+    this.setState({
+      form: dato,
+      modalConfirmacion: true
+    });
+    
+  };
+
+  cerrarConfirmacion = () =>{
+    this.setState({ modalConfirmacion: false });
+  }
+//#endregion
+  
+  //#region CRUD
+  insertar = () =>{
+    let valorNuevo = this.state.form;
+    valorNuevo.id = this.state.data.length + 1;
+    let lista = this.state.data;
+    lista.push(valorNuevo)
+    this.setState({data: lista, modalInsertar: false})
+  }
+  
+  editar = (dato) =>{
+    let contador = 0;
+    let lista = this.state.data;
+    lista.map((registro)=>{
+      if(dato.id === registro.id){
+        lista[contador].nombre = dato.nombre;
+        lista[contador].telefono = dato.telefono;
+      }
+      contador++;
+    });
+    this.setState({data: lista, modalActualizar: false})
+  }
+
+  eliminar = (dato) =>{
+    let contador = 0; 
+    let lista = this.state.data;
+    lista.map((registro) => { 
+        if(registro.id === dato.id){
+          lista.splice(contador, 1);
+        }
+         contador++;
+      });
+      this.setState({data: lista, modalConfirmacion: false});
+  }
+
+  filtrar = (e) =>{
+    const { value } = e.target;
+    let lista = this.state.data;
+    const filtered = lista.filter(fltr => fltr.nombre.toLowerCase().includes(value.toLowerCase()));
+    
+    // this.setState({data: filtered});
+    this.setState({ dataSearch: !value ?  [] : filtered});   
+  }
+  //#endregion
+
+  render() {  
+    return (
+      <>     
+        <nav className="navbar navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand">Agenda de contactos</a>            
+            <form  class="d-flex">
+              <input class="form-control me-2" onChange={this.filtrar} type="search" placeholder="Search" aria-label="Search"></input> "         "               
+            </form>
+            </div>
+        </nav>        
+          
+        <Container>             
+          <Filtrados 
+            data = {this.state.dataSearch.length ? this.state.dataSearch : this.state.data}
+            eliminar = {this.mostrarConfirmacion}
+            editar = {this.mostrarModalActualizar}
+          />   
+          
+          <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Agregar contacto</Button>
+          <br />
+          <br/> 
+        </Container>
+        {/* Modal insertar */}
+        <Modal isOpen = {this.state.modalInsertar}>
+          <ModalHeader>
+            <div>
+              <h3>Insertar contacto</h3>
+            </div>       
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>id:</label> 
+              <input className="form-control" readOnly type= "text" value = {this.state.data.length+1}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Nombre</label> 
+              <input className="form-control" name="nombre" type= "text" onChange={this.handleChange}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Email</label> 
+              <input className="form-control" name="email" type= "text" onChange={this.handleChange}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Telefono</label> 
+              <input className="form-control" name="telefono" type= "text" onChange={this.handleChange}/>
+            </FormGroup> 
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.insertar()}>Insertar</Button>
+            <Button color="danger" onClick={() => this.cerrarModalInsertar()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+
+
+        {/* Modal actualizar */}
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+            <div>
+                <h3>Editar contacto</h3>
+            </div>       
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>id:</label> 
+              <input className="form-control" readOnly type= "text" value={this.state.form.id}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Nombre</label> 
+              <input className="form-control" name="nombre" type= "text" onChange={this.handleChange} value={this.state.form.nombre}/>
+            </FormGroup> 
+
+
+            <FormGroup>
+              <label>Email</label> 
+              <input className="form-control" name="email" type= "text" onChange={this.handleChange} value={this.state.form.email}/>
+            </FormGroup> 
+         
+
+            <FormGroup>
+              <label>Telefono</label> 
+              <input className="form-control" name="telefono" type= "text" onChange={this.handleChange} value={this.state.form.telefono}/>
+            </FormGroup> 
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="primary" onClick={()=>this.editar(this.state.form)}>Editar</Button>
+            <Button color="danger" onClick={()=>this.cerrarModalActualizar()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+        <Modal isOpen = {this.state.modalConfirmacion}>
+          <ModalHeader>
+            <h4> Desea eliminar este registro? </h4>
+          </ModalHeader>
+          <ModalBody>
+            <Button color="primary" onClick={()=> this.eliminar(this.state.form)}> Si</Button> {"   "}
+            <Button color="danger" onClick={()=>this.cerrarConfirmacion()}> No</Button>
+          </ModalBody>        
+        </Modal>      
+      </>
+    );
+  }
+}
+
+export default App;
+
